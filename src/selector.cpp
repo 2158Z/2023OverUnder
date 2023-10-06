76bfcefdd8d7386dcac7853c0ab6add6c3d2f97d
jam41803 <kennysonle5.0@gmail.com>
2023-10-06 16:52:55 -0700

    Sudo tab buttons

--- src/selector.cpp
+++ src/selector.cpp
@@ -2,21 +2,36 @@
 #include "main.h"
 
 lv_obj_t *tabView;
-lv_obj_t *redBtnm;
-lv_obj_t *blueBtnm;
-lv_obj_t *skillBtnm;
-lv_obj_t *testBtnm;
+lv_obj_t *redBtn;
+lv_obj_t *blueBtn;
+lv_obj_t *skillBtn;
+lv_obj_t *testBtn;
 
 namespace selector{
     void init(){
         lv_obj_t *redTab = lv_tabview_add_tab(tabView, "Red");
         lv_obj_t *blueTab = lv_tabview_add_tab(tabView, "Blue");
         lv_obj_t *skillsTab = lv_tabview_add_tab(tabView, "Skills");
-        lv_obj_t *skillsTab = lv_tabview_add_tab(tabView, "Tests");
+        lv_obj_t *testTab = lv_tabview_add_tab(tabView, "Tests");
 
-        redBtnm = lv_btnm_create(redTab, NULL);
-        blueBtnm = lv_btnm_create(redTab, NULL);
-        skillBtnm = lv_btnm_create(redTab, NULL);
-        testBtnm = lv_btnm_create(redTab, NULL);
+        redBtn = lv_btnm_create(redTab, NULL);
+        lv_obj_set_size(redBtn, 0, 0); //TODO Change values of gui
+        lv_obj_set_pos(redBtn, 0, 0);
+        lv_obj_align(redBtn, NULL, LV_ALIGN_CENTER, 0, 0);
+
+        blueBtn = lv_btnm_create(blueTab, NULL);
+        lv_obj_set_size(blueBtn, 0, 0);
+        lv_obj_set_pos(blueBtn, 0, 0);
+        lv_obj_align(blueBtn, NULL, LV_ALIGN_CENTER, 0, 0);
+
+        skillBtn = lv_btnm_create(skillsTab, NULL);
+        lv_obj_set_size(skillBtn, 0, 0);
+        lv_obj_set_pos(skillBtn, 0, 0);
+        lv_obj_align(skillBtn, NULL, LV_ALIGN_CENTER, 0, 0);
+
+        testBtn = lv_btnm_create(testTab, NULL);
+        lv_obj_set_size(testBtn, 0, 0);
+        lv_obj_set_pos(testBtn, 0, 0);
+        lv_obj_align(testBtn, NULL, LV_ALIGN_CENTER, 0, 0);
     }
 }
