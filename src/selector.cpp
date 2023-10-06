452711211d28f1181252f5e09545acef23f0766c
jam41803 <kennysonle5.0@gmail.com>
2023-10-06 16:47:12 -0700

    Create base objects

--- src/selector.cpp
+++ src/selector.cpp
@@ -1,7 +1,22 @@
+#pragma once
 #include "main.h"
+
+lv_obj_t *tabView;
+lv_obj_t *redBtnm;
+lv_obj_t *blueBtnm;
+lv_obj_t *skillBtnm;
+lv_obj_t *testBtnm;
 
 namespace selector{
     void init(){
+        lv_obj_t *redTab = lv_tabview_add_tab(tabView, "Red");
+        lv_obj_t *blueTab = lv_tabview_add_tab(tabView, "Blue");
+        lv_obj_t *skillsTab = lv_tabview_add_tab(tabView, "Skills");
+        lv_obj_t *skillsTab = lv_tabview_add_tab(tabView, "Tests");
 
+        redBtnm = lv_btnm_create(redTab, NULL);
+        blueBtnm = lv_btnm_create(redTab, NULL);
+        skillBtnm = lv_btnm_create(redTab, NULL);
+        testBtnm = lv_btnm_create(redTab, NULL);
     }
 }
