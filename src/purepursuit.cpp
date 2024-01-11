2c9bae4d3f8f1586e286aa89de66af5575db0cdf
jam41803 <kennysonle5.0@gmail.com>
2024-01-10 18:25:01 -0600

    make purepursuit not braindead

--- src/purepursuit.cpp
+++ src/purepursuit.cpp
@@ -1,57 +1,84 @@
 #include "main.h"
-#include "auton.h"
-#include "util/odom.h"
-#include "purepursuit.h"
+#include <cmath>
 
-// Function to calculate distance between two points
-double distance(Odom odom, Point b) {
-    return sqrt(pow(b.x - odom.get_Xposition(), 2) + pow(b.y - odom.get_Yposition(), 2));
-}
+double k = 0.1;
+double look_distance = 2.0;
+double Kp = 1.0;
+double dt = 0.1;
+double WB = 2.9;
+double target_voltage = 12000.0;
 
-// Function to calculate curvature of the path at a given point
-double calculateCurvature(Odom odom, Point pathPoint) {
-    double distanceToPath = distance(odom, pathPoint);
-    return 2 * (pathPoint.x - odom.get_Xposition()) / (distanceToPath * distanceToPath);
-}
+class TargetCourse {
+public:
+    std::vector<double> cx, cy;
+    int current_index;
 
-// Function to calculate Pure Pursuit control
-double calculatePurePursuit(Odom odom, Point path[], int pathLength) {
-    int targetIndex = -1;
+    TargetCourse(std::vector<double> cx, std::vector<double> cy) : cx(cx), cy(cy), current_index(0) {}
 
-    // Find the index of the point
-    for (int i = 0; i < pathLength; i++) {
-        if (distance(odom, path[i]) < 2) {
-            targetIndex = i;
+    std::pair<int, double> search_target_index(Odom odom) {
+        double min_distance = std::numeric_limits<double>::max();
+        int nearest_point_index = current_index;
+
+        for (size_t i = current_index; i < cx.size(); i++) {
+            double dx = odom.X_position - cx[i];
+            double dy = odom.Y_position - cy[i];
+            double d = sqrt(dx * dx + dy * dy);
+            if (d < min_distance) {
+                min_distance = d;
+                nearest_point_index = i;
+            }
         }
-    }
 
-    // If point is found, calculate curvature
-    if (targetIndex != -1) {
-        double curvature = calculateCurvature(odom, path[targetIndex]);
-        // Use curvature to calculate heading
-        return atan2(1, curvature);
+        return std::make_pair(nearest_point_index, min_distance);
     }
-    return 0;
+};
+
+double proportional_control(double target, double current) {
+    return Kp * (target - current);
 }
 
-// Function to drive the robot with Pure Pursuit
-void drivePurePursuit(Odom odom, Point path[], int pathLength) {
-    double headingError = calculatePurePursuit(odom, path, pathLength) - auton::get_absolute_heading();
-    double distanceError = distance(odom, path[pathLength - 1]); // Use the last point in the path as the target
-    double turningCorrection = 0;
-    double drivingCorrection = 0;
-    auton::drive_with_voltage(12000 - turningCorrection - drivingCorrection, 12000 + turningCorrection - drivingCorrection);
+std::pair<double, int> pure_pursuit_steer_control(Odom odom, TargetCourse trajectory) {
+    int ind, _;
+    std::tie(ind, _) = trajectory.search_target_index(odom);
+
+    double tx, ty;
+    if (ind < trajectory.cx.size()) {
+        tx = trajectory.cx[ind];
+        ty = trajectory.cy[ind];
+    } else {
+        tx = trajectory.cx.back();
+        ty = trajectory.cy.back();
+        ind = trajectory.cx.size() - 1;
+    }
+
+    double alpha = atan2(ty - odom.Y_position, tx - odom.X_position) - odom.orientation_deg;
+    double delta = atan2(2.0 * WB * sin(alpha) / look_distance, 1.0);
+
+    return std::make_pair(delta, ind);
 }
 
 // int main() {
-//     // Example usage
-//     Point robotPosition = {0, 0};
-//     Point path[] = {{1, 1}, {2, 3}, {4, 2}, {6, 4}};
-//     int pathLength = sizeof(path) / sizeof(path[0]);
+//     Odom odom;
+//     std::vector<double> cx = {0.0, 10.0, 20.0, 30.0, 40.0, 50.0};
+//     std::vector<double> cy = {0.0, 10.0, -5.0, 15.0, -10.0, 5.0};
+//     TargetCourse target_course(cx, cy);
 
-//     while (true) {
-//         drivePurePursuit(robotPosition, path, pathLength);
-//         delay(20);
+//     while (1) {
+//         double adjusted_target_voltage = proportional_control(target_voltage, odom.get_Xposition());
+//         double di;
+//         int nearest_index, _;
+//         std::tie(di, nearest_index) = pure_pursuit_steer_control(odom, target_course);
+
+//         if (nearest_index == target_course.cx.size() - 1) {
+//             drive_with_voltage(0, 0);
+//             break;
+//         }
+
+//         double left_voltage = adjusted_target_voltage - WB * di / 2.0;
+//         double right_voltage = adjusted_target_voltage + WB * di / 2.0;
+
+//         drive_with_voltage(left_voltage, right_voltage);
+//         pros::delay(int(dt * 1000));
 //     }
 
 //     return 0;
