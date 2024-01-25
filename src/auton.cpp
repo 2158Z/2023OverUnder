03ef48f8b7cfc05037428b6a2f4ea46b0159eaf6
jam41803 <kennysonle5.0@gmail.com>
2024-01-25 01:53:52 -0600

    auton update

--- src/auton.cpp
+++ src/auton.cpp
@@ -188,6 +188,38 @@ namespace auton{
         rightMotorGroup.move_voltage(0);
     }
 
+    void driveTurn(float distance, float angle, float turnWeight, vector<float> tConstants = turnConstants, vector<float> dConstants = driveConstants) {
+
+        driveLeftMotorMiddle.tare_position();
+        driveRightMotorMiddle.tare_position();
+
+        float start_average_position = (get_left_position_in()+get_right_position_in())/2.0;
+        float average_position = start_average_position;
+        PID drivePID(distance, dConstants[1], dConstants[2], dConstants[3], dConstants[4], dConstants[5], dConstants[6],
+                    dConstants[7]);
+        PID turnPID(reduce_negative_180_to_180(angle - get_absolute_heading()), tConstants[1], tConstants[2], tConstants[3], tConstants[4],
+                    tConstants[5], tConstants[6], tConstants[7]);
+        while(turnPID.is_settled() == false || drivePID.is_settled() == false){
+            float error = reduce_negative_180_to_180(angle - get_absolute_heading());
+            float output = turnPID.compute(error) * 1000;
+            //printf("%f \n", error);
+            output = clamp(output, -tConstants[0], tConstants[0]);
+            
+            average_position = (get_left_position_in()+get_right_position_in())/2.0;
+            float drive_error = distance+start_average_position-average_position;
+            float drive_output = drivePID.compute(drive_error) * 1000;
+            drive_output = clamp(drive_output, -dConstants[0], dConstants[0]);
+            drive_with_voltage(((2 * turnWeight * output) + (2 * (1 - turnWeight) * drive_output)) / 2.0,
+            ((2 * turnWeight * -output) + (2 * (1 - turnWeight) * drive_output)) / 2.0);
+            // counter += 1;
+            // if (counter % 10 == 0){
+            //printf("%f \n", drive_error);
+            // }
+            delay(10);
+        }
+
+    }
+
     void driveToPoint(float X_position, float Y_position, vector<float> dConstants = driveConstants, vector<float> tConstants = turnConstants){
         PID drivePID(hypot(X_position-get_X_position(),Y_position-get_Y_position()), dConstants[1], dConstants[2], dConstants[3], dConstants[4], dConstants[5], dConstants[6],
                     dConstants[7]);
