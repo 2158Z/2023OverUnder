c288fe052784cc7b570d13d01015af2c85d95be4
jam41803 <kennysonle5.0@gmail.com>
2024-01-27 04:09:16 -0600

    selector.h

--- include/selector.h
+++ include/selector.h
@@ -1,13 +1,21 @@
 #pragma once
 
 #include <string>
+#include "main.h"
 
 #define DEFAULT 1
-#define AUTONS "Auton1", "Auton2", "Auton3"
+#define REDAUTONS "1", "2", "3"
+#define BLUEAUTONS "-1", "-2", "-3"
 
 namespace selector{
     extern lv_obj_t *tabView;
     extern int auton;
-    static const char *b[] = {AUTONS, "", ""};
-    void init(int defaultAuton=DEFAULT, const char **autons = b);
+    extern lv_obj_t *miscTab;
+    extern lv_obj_t *cordLabel;
+    extern lv_obj_t *tempLabel;
+    extern lv_obj_t *autonLabel;
+    extern lv_obj_t *phaseLabel;
+    static const char *r[] = {REDAUTONS, ""};
+    static const char *b[] = {BLUEAUTONS, ""};
+    void init(int defaultAuton = DEFAULT, const char **redAutons = r, const char **blueAutons = b);
 }
